94fa879d4edeac4f37393f89ee9ae49336bbef45
GeorgeisaGit <no>
2019-11-28 11:16:53 +0000

    Added bare bones read and check function for token

---
+++ src/main/java/com/nationwide/controller/TokenController.java
@@ -0,0 +1,32 @@
+package com.nationwide.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nationwide.persistence.domain.Token;
+import com.nationwide.service.TokenService;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/account")
+public class TokenController {
+	
+	@Autowired
+	private TokenService service;
+	
+	@PostMapping("/saveToken/{username}")
+	public Token createToken(@PathVariable String username){
+		return service.createToken(username);
+	}
+	
+	@GetMapping("/checkToken/{token}")
+	public Token checkTokenExists(@PathVariable String token) {
+		return TokenService.checkToken(token);
+	}
+	
+}
